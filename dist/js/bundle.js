e474d838ad7cddf2761fa2ddbd573660b7ccb45f
yomna raouf <yomnaraouf99@gmail.com>
2020-06-18 21:03:46 -0700

    adding download functionality

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -1,1 +1,100 @@
-!function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,t){const n=document.querySelector(".banner-btn"),r=document.querySelector(".x-btn"),o=document.querySelector(".name-input"),u=document.querySelector(".author-name"),c=document.querySelector(".entered-name");document.querySelector(".banner");let l=0,i=0;window.onload=()=>{document.getElementById("my_audio").play()},n.addEventListener("click",()=>{document.querySelector(".banner_2").style.display="none",document.querySelector(".banner").style.cssText="opacity: 1; visibility: visible",document.querySelector(".container").style.cssText=" background:rgb(243, 247, 251)",a()}),r.addEventListener("click",()=>{document.querySelector(".banner_2").style.display="flex",document.querySelector(".banner").style.cssText="opacity: 0; visibility: hidden",document.querySelector(".container").style.cssText=" background:linear-gradient(to right, rgb(5, 85, 136), rgb(243, 247, 251));"});const a=()=>{let e="Happy Eid: \n   "+o.value,t="From Yomna Raouf";l<e.length&&(c.innerText+=e.charAt(l),l++,setTimeout(a,150)),l===e.length&&i<t.length&&(u.innerText+=t.charAt(i),i++,setTimeout(a,150))}}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/js/app.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/js/app.js":
+/*!***********************!*\
+  !*** ./src/js/app.js ***!
+  \***********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("const bannerBtn = document.querySelector('.banner-btn');\r\nconst xBtn = document.querySelector('.x-btn');\r\nconst EnteredName = document.querySelector(\".name-input\");\r\nconst Author = document.querySelector('.author-name');\r\nconst Name = document.querySelector('.entered-name')\r\nconst loadBtn = document.querySelector('.load');\r\nlet i = 0;\r\nlet j = 0;\r\n\r\nwindow.onload= () => {\r\n    document.getElementById(\"my_audio\").play();\r\n}\r\n\r\nfunction draw(text_title) {\r\n  var ctx = document.getElementById('canvas').getContext('2d');\r\n  var img = new Image();\r\n  img.onload = function() {\r\n    ctx.drawImage(img, 0, 0, 700, 700);\r\n    ctx.fillText(\"Happy Eid:\", 300, 200);\r\n    ctx.fillText(  text_title , 350, 260);\r\n    ctx.fillText( \"From Yomna Raouf\" , 330, 650);\r\n    ctx.fillStyle = \"#b71c1c\";\r\n    ctx.textBaseline = '#b71c1c';\r\n    ctx.font = \"35px 'Roboto Condensed'\";\r\n  };\r\n  img.src = '../assets/eid.jpg';\r\n}\r\n\r\nloadBtn.addEventListener('click', () => {\r\n    let temp = canvas.toDataURL();\r\n    const link = document.createElement('a');\r\n    document.body.appendChild(link);\r\n    link.setAttribute('download', 'HappyEid.png');\r\n    link.href = temp;\r\n    link.click();\r\n   // console.log(temp);\r\n});\r\n\r\n\r\nbannerBtn.addEventListener('click', () => {\r\n    document.querySelector('.banner_2').style.display = 'none';\r\n    document.querySelector('.banner').style.cssText = 'opacity: 1; visibility: visible';\r\n    document.querySelector('.container').style.cssText = \" background:rgb(243, 247, 251)\"\r\n    typing();\r\n});\r\n\r\nxBtn.addEventListener('click', () => {\r\n    document.querySelector('.banner_2').style.display = 'flex';\r\n    document.querySelector('.banner').style.cssText = 'opacity: 0; visibility: hidden';\r\n    document.querySelector('.container').style.cssText = ' background:linear-gradient(to right, rgb(5, 85, 136), rgb(243, 247, 251));';\r\n});\r\n\r\nconst typing = () => {\r\n    let userName = `Happy Eid: \\n   ${EnteredName.value}` \r\n    let author = `From Yomna Raouf`;   \r\n    if (i < userName.length) {\r\n        Name.innerText += userName.charAt(i);\r\n        i++;\r\n        setTimeout(typing, 150);\r\n    }  \r\n    if ( (i === userName.length) && (j < author.length) ) {\r\n        Author.innerText += author.charAt(j);\r\n        j++;\r\n        setTimeout(typing, 150);\r\n    } \r\n    draw( `${EnteredName.value}`);\r\n}\r\n\n\n//# sourceURL=webpack:///./src/js/app.js?");
+
+/***/ })
+
+/******/ });
--- src/js/app.js
+++ src/js/app.js
@@ -3,13 +3,39 @@ const xBtn = document.querySelector('.x-btn');
 const EnteredName = document.querySelector(".name-input");
 const Author = document.querySelector('.author-name');
 const Name = document.querySelector('.entered-name')
-const Image = document.querySelector('.banner');
+const loadBtn = document.querySelector('.load');
 let i = 0;
 let j = 0;
 
 window.onload= () => {
     document.getElementById("my_audio").play();
 }
+
+function draw(text_title) {
+  var ctx = document.getElementById('canvas').getContext('2d');
+  var img = new Image();
+  img.onload = function() {
+    ctx.drawImage(img, 0, 0, 700, 700);
+    ctx.fillText("Happy Eid:", 300, 200);
+    ctx.fillText(  text_title , 350, 260);
+    ctx.fillText( "From Yomna Raouf" , 330, 650);
+    ctx.fillStyle = "#b71c1c";
+    ctx.textBaseline = '#b71c1c';
+    ctx.font = "35px 'Roboto Condensed'";
+  };
+  img.src = '../assets/eid.jpg';
+}
+
+loadBtn.addEventListener('click', () => {
+    let temp = canvas.toDataURL();
+    const link = document.createElement('a');
+    document.body.appendChild(link);
+    link.setAttribute('download', 'HappyEid.png');
+    link.href = temp;
+    link.click();
+   // console.log(temp);
+});
+
 
 bannerBtn.addEventListener('click', () => {
     document.querySelector('.banner_2').style.display = 'none';
@@ -36,6 +62,6 @@ const typing = () => {
         Author.innerText += author.charAt(j);
         j++;
         setTimeout(typing, 150);
-    }
+    } 
+    draw( `${EnteredName.value}`);
 }
-
